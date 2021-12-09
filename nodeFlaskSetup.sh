095274035e3678af974a6742af67ee997a202c4e
ctrinh <ctrinh@bu.edu>
2021-12-08 22:15:06 -0500

    Updated shell script for Flask

--- nodeFlaskSetup.sh
+++ nodeFlaskSetup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
+npm install
+cd api
+python -m venv venv
+source venv/bin/activate
+pip install flask python-dotenv
